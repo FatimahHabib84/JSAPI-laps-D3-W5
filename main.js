a0be20b2bbe9cba65f9a9f9e35a45271fe5a9470
FatimahHabib84 <fatimahhabib84@gmail.com>
2024-05-28 18:31:05 +0300

    lap-2

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 btn = document.getElementsByClassName("phone-button")
 
 btn[0].addEventListener('click',()=>{
-    document.getElementsByTagName("input")
-    window.open()
+    let lnk = document.getElementsByTagName("input")[0].value
+    window.open(lnk,'newwin','width=375,height=812');
+})
+btn[1].addEventListener('click',()=>{
+    let lnk = document.getElementsByTagName("input")[0].value
+    window.open(lnk,'newwin','width=412,height=869');
+})
+btn[2].addEventListener('click',()=>{
+    let lnk = document.getElementsByTagName("input")[0].value
+    window.open(lnk,'newwin','width=1024,height=1366');
+})
+btn[3].addEventListener('click',()=>{
+    let lnk = document.getElementsByTagName("input")[0].value
+    window.open(lnk,'newwin','width=1920,height=1018');
 })
 
 // 
